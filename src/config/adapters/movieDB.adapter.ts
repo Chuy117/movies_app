6710ccefdfb58c717ef73ef196d319b90e0652c5
Chuy117 <cruzgz.jesus@gmail.com>
2024-12-19 13:42:38 -0600

    cambios para env

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'ff585734489d3edbe38b7ef399e6b078',
+        api_key: THE_MOVIE_DB_KEY ?? 'no-key',
         language: 'es',
     }
 })

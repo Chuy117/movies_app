5472aacccd3c1f05b098d0c9048700b3b3cc7290
Chuy117 <cruzgz.jesus@gmail.com>
2024-12-19 13:26:40 -0600

    adapter change

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -3,7 +3,7 @@ import { AxiosAdapter } from './http/axios.adapter';
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'ff585734489d3edbe38b7ef399e6b078',
+        api_key: '',
         language: 'es',
     }
 })
